f4b4f898d41b693431edcee8bbc25a99841c77bf
klillenb <kertlillenberk@gmail.com>
2023-02-21 21:12:35 +0200

    Revert "Merge branch 'main' into 4-other-content-elements"
    This reverts commit 6403d8d1a45862faeac818fec348bd02accd26c9, reversing
    changes made to 5654fd8c06cedcdc3057b9945acd99511732787d.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import "./style/App.css";
 import { Box } from "@mui/material";
 import Partners from "./components/Partners";
-import Navbar from "./Components/Navbar.jsx";
 
 export default function App() {
 	return (
 		<Box>
 			<Partners />
-			<Navbar />
 		</Box>
 	);
 }
