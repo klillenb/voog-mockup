903f28d4552470c3f3368266c165232588e58f99
klillenb <kertlillenberk@gmail.com>
2023-02-21 23:51:07 +0200

    partners component - styling broken
    Nuked my old branch, idk what happened

---
+++ src/components/Partners.jsx
@@ -0,0 +1,23 @@
+import { Box } from "@mui/material";
+import "./styles/Partners.css";
+
+export default function Partners() {
+	return (
+		<Box className="partners-wrapper">
+			<Box sx={{ minWidth: "initial", maxWidth: "initial" }}>
+				<h5>Ühine teiste edukate Voo kasutajatega.</h5>
+			</Box>
+			<Box className="partners-row">
+				<a class="partners" href="https://guardtime.com">
+					<img src="src/assets/guardtime.svg" />
+				</a>
+				<a class="partners" href="https://playtech.ee/">
+					<img src="src/assets/playtech.svg" />
+				</a>
+				<a class="partners" href="https://cleveron.ee/">
+					<img src="src/assets/cleveron.svg" />
+				</a>
+			</Box>
+		</Box>
+	);
+}
