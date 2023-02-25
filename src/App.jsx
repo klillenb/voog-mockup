a92c9e28898a2afe426a3991564a6a7147d6f347
klillenb <kertlillenberk@gmail.com>
2023-02-25 22:48:01 +0200

    "Lihtne muuta" osa
    stiil pole veel päris sama

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Box } from "@mui/material";
 import Partners from "./components/Partners";
 import Navbar from "./Components/Navbar.jsx";
 import Videoplayer from "./components/Videoplayer";
+import EasyEdit from "./components/EasyEdit";
 
 export default function App() {
 	return (
@@ -11,6 +12,7 @@ export default function App() {
 			<Box className="main-body">
 				<Videoplayer />
 				<Partners />
+				<EasyEdit />
 			</Box>
 		</Box>
 	);
