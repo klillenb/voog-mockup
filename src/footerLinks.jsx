84bc015f58817b6cdd6b79a0e8b9d8d035b8c24b
klillenb <kertlillenberk@gmail.com>
2023-04-04 22:22:49 +0300

    footeri laadne toode

---
+++ src/footerLinks.jsx
@@ -0,0 +1,78 @@
+export const footerLinks = [
+	{
+		title: "Toode",
+		links: [
+			{
+				name: "Loo e-pood",
+				link: "https://www.voog.com/veebipood",
+			},
+			{
+				name: "Loo uus veebileht",
+				link: "https://www.voog.com/veebid",
+			},
+			{
+				name: "Loo blogi",
+				link: "https://www.voog.com/blogid",
+			},
+			{
+				name: "Integratsioonid",
+				link: "https://www.voog.com/integratsioonid",
+			},
+			{
+				name: "Võimalused",
+				link: "https://www.voog.com/featuurid",
+			},
+			{
+				name: "Hinnad",
+				link: "https://www.voog.com/hinnad",
+			},
+		],
+	},
+	{
+		title: "Ettevõte",
+		links: [
+			{
+				name: "Voog",
+				link: "https://www.voog.com/voost",
+			},
+			{
+				name: "Partnerlusprogramm",
+				link: "https://www.voog.com/developers/affiliate",
+			},
+			{
+				name: "Blogi",
+				link: "https://www.voog.com/blogi",
+			},
+		],
+	},
+	{
+		title: "Tugi",
+		links: [
+			{
+				name: "Tugi ja juhendid",
+				link: "https://www.voog.com/tugi",
+			},
+			{
+				name: "Võta ühendust",
+				link: "https://www.voog.com/tugi/kontakt",
+			},
+		],
+	},
+	{
+		title: "Sotsiaalmeedia",
+		links: [
+			{
+				name: "Facebook",
+				link: "https://www.facebook.com/govoog/",
+			},
+			{
+				name: "Twitter",
+				link: "https://twitter.com/govoog",
+			},
+			{
+				name: "Instagram",
+				link: "https://www.instagram.com/govoog/",
+			},
+		],
+	},
+];
