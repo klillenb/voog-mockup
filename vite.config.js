d30e41f1cca0d772a119c2026790df4871561be9
klillenb <kertlillenberk@gmail.com>
2023-04-24 22:29:56 +0300

    adjustments

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from "vite";
-import path from "path";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	base: "/voog-mockup/",
-	resolve: {
-		alias: {
-			"./": path.resolve(__dirname, "/src"),
-		},
-	},
 });
